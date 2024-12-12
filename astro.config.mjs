a109ec06f0ac159ec4c86970ed95c74b1844c767
JustBecerra <justj.becerra@gmail.com>
2024-12-12 16:34:27 -0300

    add serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel"
+import vercel from "@astrojs/vercel/serverless"
 
 import react from "@astrojs/react"
 
