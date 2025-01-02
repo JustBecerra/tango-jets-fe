3d8eba293ec032b49a9ba419626b28f8d3afe3b5
JustBecerra <justj.becerra@gmail.com>
2025-01-02 13:47:27 -0300

    remove arrival time

--- src/components/modals/ModalFlightAdd.tsx
+++ src/components/modals/ModalFlightAdd.tsx
@@ -25,9 +25,6 @@ const ModalFlightAdd: React.FC = () => {
 		const name = getCookie("username")
 		const transformedFlightData = {
 			launchtime: new Date(flightData.launchtime as string).toISOString(),
-			arrivaltime: new Date(
-				flightData.arrivaltime as string
-			).toISOString(),
 			to: flightData.to as string,
 			from: flightData.from as string,
 			airship_name: flightData.airship_title,
@@ -109,42 +106,6 @@ const ModalFlightAdd: React.FC = () => {
 									<div className="h-fit mb-6 grid grid-cols-1 gap-6 sm:grid-cols-2">
 										<div>
 											<label
-												htmlFor="launchtime"
-												className="block text-sm font-medium text-gray-900 dark:text-gray-200"
-											>
-												Launch Time
-											</label>
-											<input
-												type="datetime-local"
-												id="launchtime"
-												name="launchtime"
-												min={new Date()
-													.toISOString()
-													.slice(0, 16)}
-												className="block w-full px-4 py-2 mt-1 text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-												required
-											/>
-										</div>
-										<div>
-											<label
-												htmlFor="arrivaltime"
-												className="block text-sm font-medium text-gray-900 dark:text-gray-200"
-											>
-												Arrival Time
-											</label>
-											<input
-												type="datetime-local"
-												id="arrivaltime"
-												name="arrivaltime"
-												min={new Date()
-													.toISOString()
-													.slice(0, 16)}
-												className="block w-full px-4 py-2 mt-1 text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-												required
-											/>
-										</div>
-										<div>
-											<label
 												htmlFor="to"
 												className="block text-sm font-medium text-gray-900 dark:text-gray-200"
 											>
@@ -169,6 +130,24 @@ const ModalFlightAdd: React.FC = () => {
 												type="text"
 												id="from"
 												name="from"
+												className="block w-full px-4 py-2 mt-1 text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+												required
+											/>
+										</div>
+										<div>
+											<label
+												htmlFor="launchtime"
+												className="block text-sm font-medium text-gray-900 dark:text-gray-200"
+											>
+												Launch Time
+											</label>
+											<input
+												type="datetime-local"
+												id="launchtime"
+												name="launchtime"
+												min={new Date()
+													.toISOString()
+													.slice(0, 16)}
 												className="block w-full px-4 py-2 mt-1 text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 												required
 											/>
