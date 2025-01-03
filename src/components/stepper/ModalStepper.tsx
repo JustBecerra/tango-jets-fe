e1b1ef647df7095cbb7314a4cf9d8e63974defb5
JustBecerra <justj.becerra@gmail.com>
2025-01-03 14:16:13 -0300

    add correct behaviour of stepper colors and logos

--- src/components/stepper/ModalStepper.tsx
+++ src/components/stepper/ModalStepper.tsx
@@ -6,12 +6,11 @@ export const ModalStepper = ({ phase }: Props) => {
 	return (
 		<ol className="flex items-center w-[90%] text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
 			<li
-				className={`flex md:w-full items-center ${
-					phase === "first" && "text-blue-600 dark:text-blue-500"
-				} sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700`}
+				className={`flex md:w-full items-center text-blue-600 dark:text-blue-500
+				 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700`}
 			>
 				<span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-					{phase === "first" && (
+					{phase !== "first" && (
 						<svg
 							className="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
 							aria-hidden="true"
@@ -28,11 +27,11 @@ export const ModalStepper = ({ phase }: Props) => {
 			</li>
 			<li
 				className={`flex md:w-full items-center ${
-					phase === "second" && "text-blue-600 dark:text-blue-500"
+					phase !== "first" && "text-blue-600 dark:text-blue-500"
 				} sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700`}
 			>
 				<span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
-					{phase === "second" && (
+					{phase === "third" && (
 						<svg
 							className="w-3.5 h-3.5 sm:w-4 sm:h-4 me-2.5"
 							aria-hidden="true"
@@ -47,7 +46,11 @@ export const ModalStepper = ({ phase }: Props) => {
 				</span>
 			</li>
 			<li className="flex items-center">
-				<span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+				<span
+					className={`flex items-center ${
+						phase === "third" && "text-blue-600 dark:text-blue-500"
+					} after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500`}
+				>
 					Confirmation
 				</span>
 			</li>
