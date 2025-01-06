468b93096395fc0ac3cd13d3bfbc8351b00be0f9
JustBecerra <justj.becerra@gmail.com>
2025-01-06 16:26:48 -0300

    add initial config for zustand store

---
+++ src/store/store.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand"
+import type { Airship, Client, Flight } from "../components/table/TableModal"
+
+type State = {
+	airships: Airship[]
+	flights: Flight[]
+	clients: Client[]
+}
+
+type Action = {
+	updateAirships: (airships: State["airships"]) => void
+	updateFlights: (flights: State["flights"]) => void
+	updateClients: (clients: State["clients"]) => void
+}
+
+const useStore = create<State & Action>((set) => ({
+	airships: [],
+	flights: [],
+	clients: [],
+	updateAirships: (airships) => set(() => ({ airships: airships })),
+	updateFlights: (flights) => set(() => ({ flights: flights })),
+	updateClients: (clients) => set(() => ({ clients: clients })),
+}))
+
+export default useStore
