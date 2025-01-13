07d843cdad2bdbab7968cf7d6a468f2e9ecddd1a
JustBecerra <justj.becerra@gmail.com>
2025-01-13 09:56:36 -0300

    add warning to the chepibe

--- src/components/buttons/StepperButtons.tsx
+++ src/components/buttons/StepperButtons.tsx
@@ -71,6 +71,7 @@ export const StepperButtons = ({ phase, setPhase, operation }: props) => {
 					>
 						Back
 					</button>
+					<p>This will send an email to the master passenger</p>
 					<button
 						type="submit"
 						className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
@@ -92,6 +93,9 @@ export const StepperButtons = ({ phase, setPhase, operation }: props) => {
 					>
 						Back
 					</button>
+					<p>
+						This will send an email to the master passenger
+					</p>
 					<button
 						type="submit"
 						className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
