cd32507b7e3149e2c38a0104fe9006fc84392fb1
JustBecerra <justj.becerra@gmail.com>
2025-01-29 16:38:39 -0300

    remove edge middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: vercel({ edgeMiddleware: true }),
+	adapter: vercel(),
 	output: "server",
 	integrations: [react(), tailwind()],
 })
