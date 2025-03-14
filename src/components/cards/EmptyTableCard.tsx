9ec887c03f1dcc36508cd44f1921c601907b0eee
JustBecerra <justj.becerra@gmail.com>
2025-03-14 15:44:53 -0300

    add correct condition

--- src/components/cards/EmptyTableCard.tsx
+++ src/components/cards/EmptyTableCard.tsx
@@ -1,26 +1,23 @@
-import LoaderSpinner from "../Loaders/LoaderSpinner";
-import type { DataType } from "../table/TableModal"
+import LoaderSpinner from "../Loaders/LoaderSpinner"
 
 interface Props {
-	searchTerm: string
 	loading: boolean
 }
 
-export const EmptyTableCard = ({ searchTerm, loading }: Props) => {
-	return (
-		<>
-			{searchTerm === "" && loading ? (
-				<div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-xl">
-					<LoaderSpinner />
-				</div>
-			) : (
-				<div className="flex flex-col items-center justify-center min-h-[400px]">
-					<img src="/rafa.JPG" alt="Rafa" className="mb-4" />
-					<p className="text-zinc-400 text-lg">
-						The Trip you’re looking for can’t be found.
-					</p>
-				</div>
-			)}
-		</>
-	)
+export const EmptyTableCard = ({ loading }: Props) => {
+	if (loading) {
+		return (
+			<div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-xl">
+				<LoaderSpinner />
+			</div>
+		)
+	} else
+		return (
+			<div className="flex flex-col items-center justify-center min-h-[400px]">
+				<img src="/rafa.JPG" alt="Rafa" className="mb-4" />
+				<p className="text-zinc-400 text-lg">
+					The Trip you’re looking for can’t be found.
+				</p>
+			</div>
+		)
 }
