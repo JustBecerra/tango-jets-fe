d729d7c32beeaad4aca31ad4b68f839f69f5398a
JustBecerra <justj.becerra@gmail.com>
2025-03-25 14:37:54 -0300

    add condition for lines on map

--- src/components/cards/MapCard.tsx
+++ src/components/cards/MapCard.tsx
@@ -6,6 +6,7 @@ interface props {
 	}[]
 }
 export const MapCard = ({ Coordinates }: props) => {
+	console.log({ Coordinates })
 	return (
 		<div className="w-full sm:w-[50%] h-[800px] sm:h-full p-4 border-2 rounded flex flex-col items-center border-solid bg-white border-gray-300">
 			<div className="flex justify-center items-center w-full border-b-2 border-gray-300 border-solid">
