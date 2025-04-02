2a28a088131d79cbc89f15c2f0dfeac13ad6d55c
JustBecerra <justj.becerra@gmail.com>
2025-04-02 18:15:05 -0300

    add progress stepper main structure

--- src/components/stepper/StepperStructure.tsx
+++ src/components/stepper/StepperStructure.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { StepperHeader } from './StepperHeader'
 import type { Airship, Client, Flight } from '../table/TableModal'
+import { ProgressStepper } from "./ProgressStepper"
 
 interface props {
-    currentFlight: Flight
-    listAirships: Airship[]
-    listClients: Client[]
+	currentFlight: Flight
+	listAirships: Airship[]
+	listClients: Client[]
 }
 
-export const StepperStructure = ({currentFlight, listAirships, listClients} : props) => {
-    const lead_passenger = listClients.find((client) => client.fullname === currentFlight.master_passenger) || "John Doe"
-  return (
-    <div className="flex h-full w-full justify-center items-center gap-8"><StepperHeader currentFlight={currentFlight} lead_passenger={lead_passenger} /></div>
-  )
+export const StepperStructure = ({
+	currentFlight,
+	listAirships,
+	listClients,
+}: props) => {
+	const lead_passenger =
+		listClients.find(
+			(client) => client.fullname === currentFlight.master_passenger
+		) || "John Doe"
+	return (
+		<div className="flex flex-col h-full w-full justify-center items-center gap-8">
+			<StepperHeader
+				currentFlight={currentFlight}
+				lead_passenger={lead_passenger}
+			/>{" "}
+			<ProgressStepper />
+		</div>
+	)
 }
