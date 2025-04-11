6b5588782e28d10b123bb4f7c2192dbffa51dbe4
JustBecerra <justj.becerra@gmail.com>
2025-04-11 11:38:20 -0300

    add more styles from home

--- src/components/Home/DepartingSoon.tsx
+++ src/components/Home/DepartingSoon.tsx
@@ -73,10 +73,10 @@ const DepartingSoon = ({ flights }: props) => {
 									(window.location.href = `/trip/${flight.id}`)
 								}
 							>
-								<td className="w-1/3 px-6 py-4 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+								<td className="w-1/3 px-6 py-4 text-center text-xs font-medium text-gray-900">
 									{flight.airship_name}
 								</td>
-								<td className="w-1/3 px-6 py-4 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+								<td className="w-1/3 px-6 py-4 text-center text-xs font-medium text-gray-900">
 									{flight.from}
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
@@ -88,7 +88,7 @@ const DepartingSoon = ({ flights }: props) => {
 									</svg>
 									{flight.to}
 								</td>
-								<td className="w-1/3 px-6 py-4 text-center whitespace-nowrap text-xs font-medium text-gray-900">
+								<td className="w-1/3 px-6 py-4 text-center text-xs font-medium text-gray-900">
 									{formatLaunchTime(flight.launchtime)}
 								</td>
 							</tr>
